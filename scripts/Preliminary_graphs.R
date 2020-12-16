82bc546ea73458a7098794aa40929bf7bb2fd85d
Mohamed Henriques <mhenriquesbalde@gmail.com>
2020-12-16 01:15:25 +0000

    lkhkjgkvjk

--- scripts/Preliminary_graphs.R
+++ scripts/Preliminary_graphs.R
@@ -13,17 +13,34 @@ DB68<-read.table("data_out/db/Final_DB_class1_density_polyexcl_20201208.csv",hea
 
 ### include column with cut for most abundant families
 DB7<-aggregate(DB67$dens,by=list(family=DB67$family),FUN=mean)
+colnames(DB7)[2]<-"dens"
 
-DB7$cut<-ifelse(DB7$x>=10,"Y","N")
+DB7$cut<-ifelse(DB7$dens>=10,"Y","N")
+
+colnames(DB67)[8]<-"numb"
 DB8<-merge(DB67,DB7,by="family",all.x=T)
+colnames(DB8)
+DB8$dens.y<-NULL
+colnames(DB8)[9]<-"dens"
+
+### include column with cut for most abundant low_taxa
+DB77<-aggregate(DB66$dens,by=list(low_taxa=DB66$low_taxa),FUN=mean)
+colnames(DB77)[2]<-"dens"
+
+DB77$cut<-ifelse(DB77$dens>=50,"Y","N")
+
+DB88<-merge(DB66,DB77,by="low_taxa",all.x=T)
+colnames(DB88)
+DB88$dens.y<-NULL
+colnames(DB88)[10]<-"dens"
 
 ##Order site by island
-DB8$site1<-factor(DB8$site,levels=c("AD","BI","BR","E","A","AB"))
+DB88$site1<-factor(DB88$site,levels=c("AD","BI","BR","E","A","AB"))
 
 
 ####plots de densidade total (todos os sitios todos os meses)
 
-ggplot(DB8[DB8$cut=="Y",],aes(x=dens,y=reorder(low_taxa, dens),col=class1)) +
+ggplot(DB88[DB88$cut=="Y",],aes(x=dens,y=reorder(low_taxa, dens),col=class1)) +
   #geom_point(size=3)+
   stat_summary(size=1)+
   theme_bw() +
@@ -97,7 +114,7 @@ ggplot(DB68, aes(x=dens,y=reorder(class1, dens),fill=site)) +
 
 ### Barras normais
 
-ggplot(DB8[DB8$cut=="Y",], aes(x=dens,y=reorder(low_taxa, dens),fill=site1)) +
+#ggplot(DB88[DB88$cut=="Y",], aes(x=dens,y=reorder(low_taxa, dens),fill=site1)) +
   stat_summary(geom="bar",size=1)+
   scale_fill_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
   theme_bw() +
@@ -118,12 +135,12 @@ XXX<-dcast(data=XX,family~site1)
 YY<-aggregate(DB66$dens,by=list(low_taxa=DB66$low_taxa,site1=DB66$site),FUN=mean)
 colnames(YY)[3]<-"dens"
 
-DB77<-aggregate(DB61$dens,by=list(low_taxa=DB61$low_taxa),FUN=mean)
+#DB77<-aggregate(DB61$dens,by=list(low_taxa=DB61$low_taxa),FUN=mean)
 YY1<-merge(YY,DB66,by="low_taxa", all.x=T)
 
 YYY<-dcast(data=YY,low_taxa~site1)
 
-ggplot(XX1[XX1$cut=="Y",],aes(x=dens,y=reorder(family, dens),fill=site1)) +
+ggplot(XX1[XX1$cut=="Y",],aes(x=dens.x,y=reorder(family, dens.x),fill=site1)) +
   #stat_summary(geom="bar",size=1)+
   geom_bar(stat="identity")+
   scale_fill_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
@@ -149,7 +166,7 @@ ggplot(DB8[DB8$cut=="Y",], aes(x=dens,y=reorder(family, dens),col=site1)) +
         panel.grid.major.y = element_line(colour="grey60", linetype="dashed"),
         axis.title = element_text(size=16,face="bold"))
 
-ggplot(DB8,aes(y=dens,x=reorder(class1, -dens),fill=site1)) +
+ggplot(DB88,aes(y=dens,x=reorder(class1, -dens),fill=site1)) +
   stat_summary(geom="bar",size=1,position="dodge")+
   scale_fill_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
   theme_bw() +
@@ -165,7 +182,7 @@ ggplot(DB8,aes(y=dens,x=reorder(class1, -dens),fill=site1)) +
 ### 
 
 
-ggplot(DB6, aes(x=dens,y=reorder(family, dens))) +
+ggplot(DB8, aes(x=dens,y=reorder(family, dens))) +
   #geom_point(size=3)+
   stat_summary(size=1)+
   facet_grid(. ~ site,scales="free")+
@@ -177,7 +194,7 @@ ggplot(DB6, aes(x=dens,y=reorder(family, dens))) +
         panel.grid.minor.x = element_blank(),
         panel.grid.major.y = element_line(colour="grey60", linetype="dashed"))
 
-ggplot(DB6, aes(x=dens,y=reorder(class1, dens)))+
+ggplot(DB88, aes(x=dens,y=reorder(class1, dens)))+
   stat_summary(size=1)+
   facet_grid(. ~ site,scales="free")+
   theme_bw() +
@@ -192,32 +209,35 @@ ggplot(DB6, aes(x=dens,y=reorder(class1, dens)))+
 
 
 
-DB8$time<-paste(DB8$year,DB8$month,sep="")
-DB8$time1<-factor(DB8$time, levels=c("20181","20182","20183","201810","201811","201812","20191","20192","20193","20194","201910","20201","20202","20203"))
-DB8$time2<-ifelse(DB8$time1=="20181","01/01/2018",ifelse(DB8$time1=="20182","01/02/2018",ifelse(DB8$time1=="20183","01/03/2018",
-                ifelse(DB8$time1=="201810","01/10/2018",ifelse(DB8$time1=="201811","01/11/2018",ifelse(DB8$time1=="201812","01/12/2018",
-                ifelse(DB8$time1=="20191","01/01/2019",ifelse(DB8$time1=="20192","01/02/2019",ifelse(DB8$time1=="20193","01/03/2019",ifelse(DB8$time1=="20194","01/04/2019",
-                ifelse(DB8$time1=="201910","01/10/2019",ifelse(DB8$time1=="20201","01/01/2020",ifelse(DB8$time1=="20202","01/02/2020",
-                ifelse(DB8$time1=="20203","01/03/2020",NA))))))))))))))
+DB68$time<-paste(DB68$year,DB68$month,sep="")
+DB68$time1<-factor(DB68$time, levels=c("20181","20182","20183","201810","201811","201812","20191","20192","20193","20194","201910","20201","20202","20203"))
+DB68$time2<-ifelse(DB68$time1=="20181","01/01/2018",ifelse(DB68$time1=="20182","01/02/2018",ifelse(DB68$time1=="20183","01/03/2018",
+                ifelse(DB68$time1=="201810","01/10/2018",ifelse(DB68$time1=="201811","01/11/2018",ifelse(DB68$time1=="201812","01/12/2018",
+                ifelse(DB68$time1=="20191","01/01/2019",ifelse(DB68$time1=="20192","01/02/2019",ifelse(DB68$time1=="20193","01/03/2019",ifelse(DB68$time1=="20194","01/04/2019",
+                ifelse(DB68$time1=="201910","01/10/2019",ifelse(DB68$time1=="20201","01/01/2020",ifelse(DB68$time1=="20202","01/02/2020",
+                ifelse(DB68$time1=="20203","01/03/2020",NA))))))))))))))
 
-DB8$time3<-as.POSIXct(DB8$time2,format="%d/%m/%Y",tz="GMT")
+DB68$time3<-as.POSIXct(DB68$time2,format="%d/%m/%Y",tz="GMT")
 
-DB8$month1<-factor(DB8$month, levels=c("10","11","12","1","2","3","4"))
+DB68$month1<-factor(DB68$month, levels=c("10","11","12","1","2","3","4"))
 
-class(DB8$time3)
-unique(DB8$time3)
+
+DB68$site1<-factor(DB68$site,levels=c("AD","BI","BR","E","A","AB"))
+class(DB68$time3)
+unique(DB68$time3)
+
 
 DATES<-c("JAN18","FEB18","MAR18","OCT18","NOV18","DEC18","JAN19","FEB19","MAR19","APR19","OCT19","JAN20","FEB20","MAR20")
-names(DATES)<-levels(factor(unique(DB8$time3)))
+names(DATES)<-levels(factor(unique(DB68$time1)))
 SITES<-c("Adonga","Bijante","Bruce","Escadinhas","Anrumai","Abu")
-names(SITES)<-levels(factor(unique(DB8$site1)))
+names(SITES)<-levels(factor(unique(DB68$site1)))
 #coll<-c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen")
 
 #All dates
-ggplot(DB8,aes(x=dens,y=reorder(class1, dens),col=class1)) +
+ggplot(DB68,aes(x=dens,y=reorder(class1, dens),col=class1)) +
   #geom_point(size=3)+
   stat_summary(size=1)+
-  facet_grid(time3 ~ site1,scales="free",labeller = labeller(time3=DATES,site1=SITES))+
+  facet_grid(time1 ~ site1,scales="free",labeller = labeller(time1=DATES,site1=SITES))+
   theme_bw() +
   labs(x="Density (ind.m-2)",y="Class (ordered by total density)")+
   theme(axis.text.x = element_text(size=16),
@@ -231,9 +251,9 @@ ggplot(DB8,aes(x=dens,y=reorder(class1, dens),col=class1)) +
 #Merging months
 
 DATES1<-c("OCT","NOV","DEZ","JAN","FEB","MAR","APR")
-names(DATES1)<-levels(factor(unique(DB8$month1)))
+names(DATES1)<-levels(factor(unique(DB68$month1)))
 
-ggplot(DB8, aes(x=dens,y=reorder(class1, dens),col=class1)) +
+ggplot(DB68, aes(x=dens,y=reorder(class1, dens),col=class1)) +
   #geom_point(size=3)+
   stat_summary(size=1)+
   facet_grid(month1 ~ site1,scales="free",labeller = labeller(month1=DATES1,site1=SITES))+
@@ -252,26 +272,27 @@ ggplot(DB8, aes(x=dens,y=reorder(class1, dens),col=class1)) +
 ### per site vs class
 
 
-ggplot(DB8,aes(x=month1,y=dens,col=class1,group=class1))+
+ggplot(DB68,aes(x=month1,y=dens,col=class1,group=class1))+
   stat_summary(geom="pointrange",position=position_dodge(width=0.3),size=0.7)+
   stat_summary(geom="line",lwd=1.5,alpha=0.50)+
-  facet_wrap(.~site1,scales="free",labeller = labeller(site1=SITES))+
+  facet_wrap(.~site1,labeller = labeller(site1=SITES))+
   theme_bw()+
   labs(x="Month (aggregation of all years)",y="Mean density (ind.m-2)")+
   theme(axis.text.x = element_text(size=16),axis.text.y = element_text(size=12),axis.title = element_text(size=15,face="bold"))+
   theme(strip.text = element_text(face="bold", size=rel(1.5)))
 
 
+DB88$month1<-factor(DB88$month, levels=c("10","11","12","1","2","3","4"))
 
-
-ggplot(DB8,aes(x=time3,y=dens,col=site,group=site))+
-  stat_summary(geom="pointrange",position=position_dodge(width=0.6),size=0.7)+
-  stat_summary(geom="line")+
-  facet_wrap(.~class1,scales="free")+
+ggplot(DB88[DB88$cut=="Y",],aes(x=month1,y=dens,col=low_taxa,group=low_taxa))+
+  #stat_summary(geom="pointrange",position=position_dodge(width=0.6),size=0.7)+
+  geom_smooth(geom="smooth",method="auto",se=F,size=1.5)+
+  facet_wrap(.~site1,scales="free")+
   theme_bw()+
   labs(x="Time (months of sampling)",y="mean of density (ind.m-2)")+
-  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1))+
-  scale_x_datetime(breaks=c(unique(DB6$time3)))
+  theme(axis.text.x = element_text(angle=45, hjust=1, vjust=1))
+  #scale_colour_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))
+  #scale_x_datetime(breaks=c(unique(DB68$time3)))
 
 
 ###Aggregates to check values of mean densities per site 
--- scripts/Sprichness_diversity.R
+++ scripts/Sprichness_diversity.R
@@ -3,7 +3,7 @@ graphics.off()
 rm(list=ls())
 
 ## Pacotes
-packs<-c("vegan","ggplot2","viridis","RColorBrewer","xlsx","psych","reshape2")
+packs<-c("vegan","ggplot2","viridis","RColorBrewer","xlsx","psych","reshape2","beepr")
 lapply(packs,require,character.only=T)
 
 
@@ -108,7 +108,7 @@ i1<-aggregate(db1$numb,by=list(island=db1$island,coreID=db1$coreID,low_taxa=db1$
 i2<-dcast(i1,island+coreID~low_taxa)
 
 
-set.seed(1)
+set.seed(2)
 r=1000
 test<-array(NA,c(r,length(unique(i2$island))))
 island<-as.character(unique(i2$island))
@@ -125,7 +125,7 @@ for (i in 1:length(island1)) {
     test[j,i]<-sum(s1)
   }
 }
-tot<-data.frame(island=island1,mean_num_ind=apply(test,2,mean),sd_num_ind=apply(test,2,sd),min_num_ind=apply(test,2,min),max_num_ind=apply(test,2,max))
+tot1<-data.frame(island=island1,mean_num_ind=apply(test,2,mean),sd_num_ind=apply(test,2,sd),min_num_ind=apply(test,2,min),max_num_ind=apply(test,2,max))
 
 
 
@@ -138,7 +138,7 @@ R=1000
 set.seed(3)
 island<-as.character(unique(df1$island))
 y=c("Orango","Formosa","Bubaque")
-island1<-site[order(match(island,y))]
+island1<-island[order(match(island,y))]
 g1<-array(NA, c(length(island1), 5))
 colnames(g1)<-c("island","Mean_Shannon-Wiener","SD","N_cores","N_ind")
 
@@ -155,7 +155,7 @@ for (x in 1:length(island1))
   lim<-sum(tt1)
   for (i in 2:cores) {
     print(paste("starting core",i))
-    if(lim<=3000){
+    if(lim<=1500){
       tt1[i,]<-tt[sample(1:nrow(tt), 1, replace=T),]
       lim<-sum(tt1)
     } else{
@@ -181,6 +181,338 @@ for (x in 1:length(island1))
   }
   g1[x,2]<-round(mean(rr),2)
   g1[x,3]<-round(sd(rr),2)
+}
+
+
+############################### Now selecting only samples for which we have data for the same months #########################
+unique(db1$month)
+db2<-db1[which(db1$month=="2"|db1$month=="3"),]
+unique(db2$month)
+
+###############################################################################################################################
+### Checking differences in the mean total numb ind in each site based on boostrapping 71 samples 1000 times
+t1<-aggregate(db2$numb,by=list(site=db2$site,coreID=db2$coreID,low_taxa=db2$low_taxa),FUN=sum)
+t2<-dcast(t1,site+coreID~low_taxa)
+table(t2$site)
+
+set.seed(1)
+r=1000
+test<-array(NA,c(r,length(unique(t2$site))))
+site<-as.character(unique(t2$site))
+y=c("AB","A","E","BI","BR","AD")
+site1<-site[order(match(site,y))]
+colnames(test)<-site1
+
+for (i in 1:length(site1)) {
+  print(site1[i])
+  t3<-t2[which(t2$site==site1[i]),]
+  for (j in 1:r) {
+    s<-t3[sample(1:nrow(t3),28,replace=T),]
+    s1<-apply(s[,-c(1:2)],2,sum)
+    test[j,i]<-sum(s1)
+  }
+}
+tot3<-data.frame(site=site1,mean_num_ind=apply(test,2,mean),sd_num_ind=apply(test,2,sd),min_num_ind=apply(test,2,min),max_num_ind=apply(test,2,max))
+
+
+############## Calcular diversidade Shannon-Wienner limitando o bootstrap a 71 cores e/ou 800 individuos por site
+########### Diversidade per site #################
+dd<-aggregate(db2$numb,by=list(site=db2$site,coreID=db2$coreID,low_taxa=db2$low_taxa),FUN=sum)
+dd1<-dcast(dd,site+coreID~low_taxa)
+
+R=1000
+set.seed(0)
+site<-as.character(unique(dd1$site))
+y=c("AB","A","E","BI","BR","AD")
+site1<-site[order(match(site,y))]
+d1<-array(NA, c(length(site1), 5))
+colnames(d1)<-c("site","Mean_Shannon-Wiener","SD","N_cores","N_ind")
+
+cores<-28
+sum(tt1)
+dens1<-function(x){x/0.00866}
+dens2<-function(x){x/0.00817}
+
+for (x in 1:length(site1))
+{
+  print(site1[x])
+  d1[x,1]<-site1[x]
+  tt<-dd1[dd1$site==site1[x],-(1:2)]
+  tt1<-tt[sample(1:nrow(tt), 1, replace=T),]
+  lim<-sum(tt1)
+  for (i in 2:cores) {
+    print(paste("starting core",i))
+    if(lim<=1000){
+      tt1[i,]<-tt[sample(1:nrow(tt), 1, replace=T),]
+      lim<-sum(tt1)
+    } else{
+      print(paste("sampling in",site1[x],"reached",lim,"ind at core",i))
+    }
+  }
+  d1[x,4]<-nrow(tt1)
+  d1[x,5]<-lim
+  print(paste("starting diversity calculations in",site1[x]))
+  ### calculating density
+  if(site1[x]=="AD"){
+    tt2<-sapply(tt1,dens1)
+  } else {
+    tt2<-sapply(tt1,dens2)
+  }
+  rr<-array(NA,R)
+  for (y in 1:R)
+  {
+    sel<-sample(1:nrow(tt2), nrow(tt2), replace=T)
+    f<-apply(tt2[sel,], 2, mean)
+    f1<-f[-c(which(f==0))]
+    rr[y]<-sum(f1/sum(f1)*log(f1/sum(f1)))*-1
+  }
+  d1[x,2]<-round(mean(rr),2)
+  d1[x,3]<-round(sd(rr),2)
+}
+
+###################### diversity estimates with increasing sample size (core) ###############################
+
+############## Calcular diversidade Shannon-Wienner limitando o bootstrap a 1 até 71 cores e/ou 800 individuos por site
+########### Diversidade per site #################
+dd<-aggregate(db1$numb,by=list(site=db1$site,coreID=db1$coreID,low_taxa=db1$low_taxa),FUN=sum)
+dd1<-dcast(dd,site+coreID~low_taxa)
+table(dd1$site)
+
+R=1000
+set.seed(0)
+site<-as.character(unique(dd1$site))
+y=c("AB","A","E","BI","BR","AD")
+site1<-site[order(match(site,y))]
+d1<-array(NA, c(length(site1), 5))
+colnames(d1)<-c("site","Mean_Shannon_Wiener","SD","N_cores","N_ind")
+myList<-vector("list",107)
+cores<-c(2:108)
+sum(tt1)
+dens1<-function(x){x/0.00866}
+dens2<-function(x){x/0.00817}
+
+for (j in cores){
+  
+  for (x in 1:length(site1))
+{
+  print(site1[x])
+  d1[x,1]<-site1[x]
+  tt<-dd1[dd1$site==site1[x],-(1:2)]
+  tt1<-tt[sample(1:nrow(tt), 1, replace=T),]
+  lim<-sum(tt1)
+  for (i in cores[1]:j) {
+    print(paste("starting core",i))
+    if(lim<=sum(db1$numb[db1$site=="AD"])){
+      tt1[i,]<-tt[sample(1:nrow(tt), 1, replace=T),]
+      lim<-sum(tt1)
+    } else{
+      print(paste("sampling in",site1[x],"reached",lim,"ind at core",i))
+    }
+  }
+  d1[x,4]<-nrow(tt1)
+  d1[x,5]<-lim
+  print(paste("starting diversity calculations in",site1[x]))
+  ### calculating density
+  if(site1[x]=="AD"){
+    tt2<-sapply(tt1,dens1)
+  } else {
+    tt2<-sapply(tt1,dens2)
+  }
+  rr<-array(NA,R)
+  for (y in 1:R)
+  {
+    sel<-sample(1:nrow(tt2), nrow(tt2), replace=T)
+    f<-apply(tt2[sel,], 2, mean)
+    f1<-f[-c(which(f==0))]
+    rr[y]<-sum(f1/sum(f1)*log(f1/sum(f1)))*-1
+  }
+  d1[x,2]<-round(mean(rr),2)
+  d1[x,3]<-round(sd(rr),2)
+  }
+
+myList[[j-1]]<-d1   
+}
+beep()
+
+
+A1<-data.frame(site=NA,Mean_Shannon_Wiener=NA,SD=NA,N_cores=NA,N_ind=NA)
+names(A1)
+
+for (i in 1:length(myList)){
+  A2<-data.frame(myList[[i]])
+  #names(A2)<-names(A1)
+  A1<-rbind(A1,A2)
+}
+
+str(A1)
+
+A1<-A1[-1,]
+unique(A1$N_cores[A1$site=="AD"])
+A1$site1<-factor(A1$site,levels=c("AD","BI","BR","E","A","AB"))
+A1$N_ind<-as.numeric(A1$N_ind)
+A1$Mean_Shannon_Wiener<-as.numeric(A1$Mean_Shannon_Wiener)
+A1$SD<-as.numeric(A1$SD)
+A1$N_cores<-as.numeric(A1$N_cores)
+
+##Plot diversity estimates varying with sample size (core) in each site
+
+ggplot(A1,aes(x=N_cores,y=Mean_Shannon_Wiener,col=site1))+
+  geom_point()+
+  stat_smooth(geom="smooth",method="loess",se=F)+
+  theme_bw()+
+  scale_colour_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
+  scale_x_continuous(breaks = seq(2,108,10))
+
+ggplot(A1,aes(x=N_ind,y=Mean_Shannon_Wiener,col=site1))+
+  geom_point()+
+  stat_smooth(geom="smooth",method="loess",se=F)+
+  theme_bw()+
+  scale_colour_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
+  scale_x_continuous(breaks = seq(0,3030,200))
+
+
+
+
+
+############## Calcular riqueza específica limitando o bootstrap a 1 até 71 cores por site
+########### Riqueza específica por site #################
+dd<-aggregate(db1$numb,by=list(site=db1$site,coreID=db1$coreID,low_taxa=db1$low_taxa),FUN=sum)
+dd1<-dcast(dd,site+coreID~low_taxa)
+table(dd1$site)
+
+
+
+R=1000
+set.seed(5)
+site<-as.character(unique(dd1$site))
+y=c("AB","A","E","BI","BR","AD")
+site1<-site[order(match(site,y))]
+d1<-array(NA, c(length(site1), 5))
+colnames(d1)<-c("site","Mean_Shannon_Wiener","SD","N_cores","N_ind")
+myList1<-vector("list",107)
+cores<-c(2:108)
+
+presabs<-function(x){ifelse(x>0,1,0)}
+
+for (j in cores){
+  
+  for (x in 1:length(site1))
+  {
+    print(site1[x])
+    d1[x,1]<-site1[x]
+    tt<-dd1[dd1$site==site1[x],-(1:2)]
+    tt1<-tt[sample(1:nrow(tt), 1, replace=T),]
+    lim<-sum(tt1)
+    for (i in cores[1]:j) {
+      print(paste("starting core",i))
+      if(lim<=sum(db1$numb[db1$site=="AD"])){
+        tt1[i,]<-tt[sample(1:nrow(tt), 1, replace=T),]
+        lim<-sum(tt1)
+      } else{
+        print(paste("sampling in",site1[x],"reached",lim,"ind at core",i))
+      }
+    }
+    d1[x,4]<-nrow(tt1)
+    d1[x,5]<-lim
+    print(paste("starting sp richness calculations in",site1[x]))
+    
+    rr<-array(NA,R)
+    for (y in 1:R)
+    {
+      sel<-sample(1:nrow(tt1), nrow(tt1), replace=T)
+      f<-apply(tt1[sel,],2,sum)
+      f1<-sapply(f,presabs)
+      rr[y]<-sum(f1)
+    }
+    d1[x,2]<-round(mean(rr),2)
+    d1[x,3]<-round(sd(rr),2)
+  }
+  
+  myList1[[j-1]]<-d1   
+}
+beep()
+
+myList1[[1]]
+B1<-data.frame(site=NA,Mean_Shannon_Wiener=NA,SD=NA,N_cores=NA,N_ind=NA)
+names(B1)
+
+for (i in 1:length(myList1)){
+  B2<-data.frame(myList1[[i]])
+  #names(A2)<-names(A1)
+  B1<-rbind(B1,B2)
+}
+
+str(B1)
+colnames(B1)[2]<-"Mean_sp_richness"
+
+B1<-B1[-1,]
+unique(B1$N_cores[B1$site=="AD"])
+B1$site1<-factor(B1$site,levels=c("AD","BI","BR","E","A","AB"))
+B1$N_ind<-as.numeric(B1$N_ind)
+B1$Mean_sp_richness<-as.numeric(B1$Mean_sp_richness)
+B1$SD<-as.numeric(B1$SD)
+B1$N_cores<-as.numeric(B1$N_cores)
+
+##Plot diversity estimates varying with sample size (core) in each site
+
+ggplot(B1,aes(x=N_cores,y=Mean_sp_richness,col=site1))+
+  geom_point()+
+  stat_smooth(geom="smooth",method="loess",se=F)+
+  theme_bw()+
+  scale_colour_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
+  scale_x_continuous(breaks = seq(2,108,10))
+
+ggplot(B1,aes(x=N_ind,y=Mean_sp_richness,col=site1))+
+  geom_point()+
+  stat_smooth(geom="smooth",method="loess",se=F)+
+  theme_bw()+
+  scale_colour_manual(values=c("red","steelblue2","royalblue3","darkblue","limegreen","darkgreen"))+
+  scale_x_continuous(breaks = seq(0,3030,200))
+
+
+#### Calculating species richness using 71 cores and a limit of 800 individuals
+R=1000
+set.seed(6)
+site<-as.character(unique(dd1$site))
+y=c("AB","A","E","BI","BR","AD")
+site1<-site[order(match(site,y))]
+s1<-array(NA, c(length(site1), 5))
+colnames(s1)<-c("site","Mean_sp_richness","SD","N_cores","N_ind")
+
+cores<-71
+
+presabs<-function(x){ifelse(x>0,1,0)}
+
+for (x in 1:length(site1))
+  {
+    print(site1[x])
+    s1[x,1]<-site1[x]
+    tt<-dd1[dd1$site==site1[x],-(1:2)]
+    tt1<-tt[sample(1:nrow(tt), 1, replace=T),]
+    lim<-sum(tt1)
+    for (i in 2:cores) {
+      print(paste("starting core",i))
+      if(lim<=800){
+        tt1[i,]<-tt[sample(1:nrow(tt), 1, replace=T),]
+        lim<-sum(tt1)
+      } else{
+        print(paste("sampling in",site1[x],"reached",lim,"ind at core",i))
+      }
+    }
+    s1[x,4]<-nrow(tt1)
+    s1[x,5]<-lim
+    print(paste("starting sp richness calculations in",site1[x]))
+    
+    rr<-array(NA,R)
+    for (y in 1:R)
+    {
+      sel<-sample(1:nrow(tt1), nrow(tt1), replace=T)
+      f<-apply(tt1[sel,],2,sum)
+      f1<-sapply(f,presabs)
+      rr[y]<-sum(f1)
+    }
+    s1[x,2]<-round(mean(rr),2)
+    s1[x,3]<-round(sd(rr),2)
 }
 
 
